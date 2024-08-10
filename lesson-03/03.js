7b0acae730b40dc6caadec9d9ce8805c461ef24d
Ruslan Shaikhin <shaikhin1996@gmail.com>
2024-08-10 19:03:54 +0200

    iteration

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -2,11 +2,11 @@
 
 function findLargest(num1, num2, num3) {
     let result;
-    if (num1>num2 && num1>num3) {
+    if (num1>=num2 && num1>=num3) {
         result = num1;
-    } else if (num2>num1 && num2>num3) {
+    } else if (num2>=num1 && num2>=num3) {
         result = num2;
-    } else if (num3>num1 && num3>num2) {
+    } else if (num3>=num1 && num3>=num2) {
         result = num3;
     }
     return result;
