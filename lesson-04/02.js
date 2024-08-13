3c80091a71c9165211bd6b645278f0da71155ac0
Ruslan Shaikhin <shaikhin1996@gmail.com>
2024-08-13 22:01:10 +0200

    attempt

--- lesson-04/02.js
+++ lesson-04/02.js
@@ -15,4 +15,15 @@
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-function findUniqueElements() {}
+
+
+
+function findUniqueElements(array) { //1, 2, 3
+    const newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if(includesElement(newArray, array[i]) === false) {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
+}
