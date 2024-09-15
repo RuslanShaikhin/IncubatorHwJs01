5f887d88d7fd8687406d8d52c5be17a15b899fb9
Ruslan Shaikhin <shaikhin1996@gmail.com>
2024-09-15 10:27:38 +0200

    attempt

--- lesson-09/02.js
+++ lesson-09/02.js
@@ -23,5 +23,18 @@ const doubledNumbers = map(numbers, (element, index) => {
 
 console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
+const numbers = [1, 2, 3, 4, 5]
 
-const map = () => {}
+const callback = (element, index) => {
+  return element * 2;
+}
+
+const map = (array, callback) => {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(callback(array[i],i));
+  }
+  return newArray;
+}
+
+console.log(map(numbers, callback));
